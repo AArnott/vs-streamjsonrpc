5ef82b204fbb1672ec2510a0775c7b6581876b58
Andrew Arnott <andrewarnott@gmail.com>
2022-03-17 09:14:37 -0600

    Start on adding analyzers

---
+++ src/StreamJsonRpc.Analyzers/Class1.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace StreamJsonRpc.Analyzers;
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public class LegalRpcInterfaceMembersAnalyzer : DiagnosticAnalyzer
+{
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+    public override void Initialize(AnalysisContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
